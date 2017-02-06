7538f952fde0ca13caa83178ec9aef8b75c5572e
Jack Sneed <JackSSS@users.noreply.github.com>
2017-02-06 14:55:30 -0800

    integrate spotify api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 import { AppComponent }  from './app.component';
 
 import {SearchComponent} from './components/search/search.component';
@@ -12,7 +13,9 @@ import {routing} from './app.routing';
 @NgModule({
   imports:      [ 
     BrowserModule,
-    routing
+    routing,
+    FormsModule,
+    HttpModule
      ],
   declarations: [ 
     AppComponent,
